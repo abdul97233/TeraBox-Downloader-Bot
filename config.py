cb98144e89cfa012e76faff12a87118296a850fe
Abdul Kalam Shekh <37735290+abdul97233@users.noreply.github.com>
2024-05-05 13:59:14 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,25 @@
+# API credentials
+API_ID = 123456
+API_HASH = "DWSC65HGFswcf56g8ergjhgdw54586"
+BOT_TOKEN = "123456:abcdekgfiJLKMNOPqjngghdf"
+
+# Redis configuration
+REDIS_CONFIG = {
+    "HOST": "redis-12345.c16.us-east-1-2.ec2.cloud.redislabs.com",
+    "PORT": 17713,
+    "USERNAME": "default",
+    "PASSWORD": "password",
+    "DATABASE": "NTM"
+}
+
+# Chat ID where videos will be stored
+PRIVATE_CHAT_ID = -100123456789
+
+# Authentication cookie
+COOKIE = {
+    "COOKIE": "Paste your cookie here"
+}
+# COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
+
+# List of bot admins
+ADMINS = [803003146]
