dc566561a0e33074e4e9036da97d3df01505084e
Abdul Kalam Shekh <37735290+abdul97233@users.noreply.github.com>
2025-12-31 16:28:02 +0545

    Update terabox.py

--- terabox.py
+++ terabox.py
@@ -57,7 +57,7 @@ def extract_surl_from_url(url: str) -> str | None:
 
 # ---------------- AURIXS API DOWNLOADER ---------------- #
 
-AURIXS_API_TEMPLATE = (
+NTM_API_TEMPLATE = (
     "https://api.ntm.com/api/terabox?key=NMTPASS&url={url}"
 )
 
